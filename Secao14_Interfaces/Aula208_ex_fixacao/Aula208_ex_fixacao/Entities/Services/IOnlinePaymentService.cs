1119c3f9b75470c6ac9abc4b6c8a216cef809f0d
Warley Afonso <warleysilva365@gmail.com>
2023-02-14 12:45:42 -0300

    Subindo exercicio da aula 208 sobre instancias

---
+++ Secao14_Interfaces/Aula208_ex_fixacao/Aula208_ex_fixacao/Entities/Services/IOnlinePaymentService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula208_ex_fixacao.Entities.Services {
+    internal interface IOnlinePaymentService {
+        public double PaymentFee (double amount);
+        public double Interest (double amount, int months);
+    }
+}
