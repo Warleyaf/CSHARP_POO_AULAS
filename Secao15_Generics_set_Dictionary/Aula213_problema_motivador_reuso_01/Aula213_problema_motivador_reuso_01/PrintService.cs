3010e37107e46147327c63fa8d7fa09d529831ad
Warley Afonso <warleysilva365@gmail.com>
2023-02-16 20:46:25 -0300

    Adicionando a secao 15 e o problema motivador 1 (reuso)

---
+++ Secao15_Generics_set_Dictionary/Aula213_problema_motivador_reuso_01/Aula213_problema_motivador_reuso_01/PrintService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Aula213_problema_motivador_reuso_01 {
+    internal class PrintService {
+        private int[] _values = new int[10];
+        private int _count = 0;
+
+        public void AddValue (int value) {
+            if (_count == 10) { // isso aqui se chama programação defensiva
+                throw new InvalidOperationException("PrintService is Full");
+            }
+
+            _values[_count] = value;
+            _count++;
+        }
+
+        public int First () {
+            if (_count == 0) { // isso aqui se chama programação defensiva
+                throw new InvalidOperationException("PrintService is empty");
+            }
+
+            return _values[0];
+        }
+
+        public void Print () {
+            Console.Write("[");
+            for (int i = 0; i < _count - 1; i++) {
+                Console.Write(_values[i] + ", ");
+            }
+            if (_count > 0) {
+                Console.WriteLine(_values[_count - 1]);
+            }
+            Console.Write("]");
+        }
+    }
+}
---
+++ Secao15_Generics_set_Dictionary/Aula213_problema_motivador_reuso_01/Aula213_problema_motivador_reuso_01/Program.cs
@@ -0,0 +1,23 @@
+﻿using Aula213_problema_motivador_reuso_01;
+using System;
+
+namespace MyApp
+{
+    internal class Program {
+        static void Main (string[] args) {
+            PrintService printService = new PrintService();
+
+            Console.Write("How many values? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i<n; i++) {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            // agora terei que imprimir todos os meus elementos
+            printService.Print();
+            Console.WriteLine($"First: " + printService.First());
+        }
+    }
+}
